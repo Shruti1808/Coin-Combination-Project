3830c5e760821260fbf5dfb78b17a1c58cc8a858
Renee Mei and Shruti Priya <renee+shruti@>
2017-02-14 11:46:22 -0800

    add HomeModule.cs and views to create a web interface for the application

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -9,7 +9,14 @@ namespace Coins
   {
     public HomeModule()
     {
-
+      Get["/"] = _ => View["index.cshtml"];
+      Get["/results"] = _ =>
+      {
+        int cents = Request.Query["coin"];
+        CoinsCombinations Change = new CoinsCombinations();
+        List<int> results = Change.Change(cents);
+        return View["results.cshtml", results];
+      };
     }
   }
 }
